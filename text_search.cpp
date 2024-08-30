668722566d614fe849e5ca6e3db8fde53e2fcbf4
K3n1-Code <k3n1.work@gmail.com>
2024-08-30 14:03:50 -0300

    computeFailFunction

--- text_search.cpp
+++ text_search.cpp
@@ -6,6 +6,55 @@
 
 using namespace std;
 
+class Matrix{
+    private:
+    vector<vector<int>> matrix;
+    int size[2];
+
+    public:
+    Matrix(int w, int h){
+        size[0]=w;
+        size[1]=h;
+        matrix=vector<vector<int>>(w);
+        for(int k=0;k<w;k++){
+            matrix[k]=vector<int>(h);
+        }
+    }
+
+    Matrix(){
+        this->matrix=vector<vector<int>>();
+        size[0]=0;
+        size[1]=0;
+    }
+
+    int width(){return size[0];}
+
+    int height(){return size[1];}
+
+    vector<int> Matrix::operator[](const int &w){
+        return matrix[w];
+    }
+};
+
+Matrix computeFailureFunction(string str){
+    Matrix func(256,str.length());
+    for(int c=0;c<256;c++){
+        func[c][0]=0;
+    }
+    func[str[0]][0]=1;
+    int x=0;
+
+    for(int k=1;k<str.length()-1;k++){
+        for(int c=0;c<256;c++){
+            func[c][k]=func[c][x];
+        }
+        func[str[k]][k]=k+1;
+        x=func[str[k]][x];
+    }
+
+    return func;
+}
+
 int main(){
     
 }
